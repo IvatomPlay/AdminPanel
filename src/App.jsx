41858826f4fc9500bbc9b9cb521b2ed5f9d7050f
Ivan Tomtosov <ivatom@MacBook-Air-Ivan.local>
2024-11-06 20:49:16 +0900

    starting before work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="app">
-      
+      <p>hello form Api.jsx</p>
     </div>
   );
 }
---
+++ src/components/dashboard/dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <p>hello from dashboard.jsx</p>
+    </div>
+  )
+}
+
+export default Dashboard;
