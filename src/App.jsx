08ee989afffd2511db61a3dd38baa974100dc6d7
Ivan Tomtosov <ivatom@MacBook-Air-Ivan.local>
2024-11-09 22:20:54 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './index.css'
+import './index.css';
+import Sidepanel from './components/scenes/sidepanel/Sidepanel';
 
 function App() {
   return (
     <div className="app">
       <p>hello form Api.jsx</p>
+      <Sidepanel />
     </div>
   );
 }
---
+++ src/components/scenes/sidepanel/sidepanel.jsx
@@ -0,0 +1,11 @@
+import './sidepanel.css';
+
+function Sidepanel() {
+  return (
+    <div className="sidepanel">
+      <p>hello from sidepanel.jsx</p>
+    </div>
+  )
+}
+
+export default Sidepanel;
